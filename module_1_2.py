3959d8405bf741a7e99cc8528302498797a0936c
Vladislav Turlyun <118107309+vladjken@users.noreply.github.com>
2024-10-01 22:57:45 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,7 @@
+done_homework_count = 12
+hours_spend = 1.5
+cours_name = "Python"
+time_for_one_homework = (hours_spend / done_homework_count)
+
+print('Курс:', cours_name, 'всего задач:', done_homework_count, 'затрачено часов:', hours_spend,'среднее время выполнения:', time_for_one_homework, 'часов')
+
