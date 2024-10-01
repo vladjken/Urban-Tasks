8c6092bb0ceffa9146e4fe41697acca298d4f03b
Vladislav Turlyun <118107309+vladjken@users.noreply.github.com>
2024-10-01 22:33:50 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Vladislav'
+print('Name: ' + name)
+age = 27
+print('Age: ' + str(age))
+age = age + 1
+print('New age: ' + str(age))
+is_student = True
+print('Is student: ' + str(is_student))
